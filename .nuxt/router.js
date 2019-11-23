a3760e7633a625cee76e8acd6a36f321026599c0
lucong <332553882@qq.com>
2019-11-23 16:54:21 +0800

    update

---
+++ .nuxt/router.js
@@ -0,0 +1,73 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import { interopDefault } from './utils'
+import scrollBehavior from './router.scrollBehavior.js'
+
+const _5243c3ae = () => interopDefault(import('../pages/auth.vue' /* webpackChunkName: "pages/auth" */))
+const _056e35b4 = () => interopDefault(import('../pages/cart.vue' /* webpackChunkName: "pages/cart" */))
+const _4d1a2ba4 = () => interopDefault(import('../pages/list.vue' /* webpackChunkName: "pages/list" */))
+const _1be0cb70 = () => interopDefault(import('../pages/payment.vue' /* webpackChunkName: "pages/payment" */))
+const _9370c7a0 = () => interopDefault(import('../pages/goods/detail.vue' /* webpackChunkName: "pages/goods/detail" */))
+const _3612672e = () => interopDefault(import('../pages/order/confirm.vue' /* webpackChunkName: "pages/order/confirm" */))
+const _02b28138 = () => interopDefault(import('../pages/order/detail.vue' /* webpackChunkName: "pages/order/detail" */))
+const _8f44da76 = () => interopDefault(import('../pages/order/list.vue' /* webpackChunkName: "pages/order/list" */))
+const _36b01db6 = () => interopDefault(import('../pages/user/address.vue' /* webpackChunkName: "pages/user/address" */))
+const _719fc8bc = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
+
+Vue.use(Router)
+
+export const routerOptions = {
+  mode: 'history',
+  base: decodeURI('/'),
+  linkActiveClass: 'nuxt-link-active',
+  linkExactActiveClass: 'nuxt-link-exact-active',
+  scrollBehavior,
+
+  routes: [{
+    path: "/auth",
+    component: _5243c3ae,
+    name: "auth"
+  }, {
+    path: "/cart",
+    component: _056e35b4,
+    name: "cart"
+  }, {
+    path: "/list",
+    component: _4d1a2ba4,
+    name: "list"
+  }, {
+    path: "/payment",
+    component: _1be0cb70,
+    name: "payment"
+  }, {
+    path: "/goods/detail",
+    component: _9370c7a0,
+    name: "goods-detail"
+  }, {
+    path: "/order/confirm",
+    component: _3612672e,
+    name: "order-confirm"
+  }, {
+    path: "/order/detail",
+    component: _02b28138,
+    name: "order-detail"
+  }, {
+    path: "/order/list",
+    component: _8f44da76,
+    name: "order-list"
+  }, {
+    path: "/user/address",
+    component: _36b01db6,
+    name: "user-address"
+  }, {
+    path: "/",
+    component: _719fc8bc,
+    name: "index"
+  }],
+
+  fallback: false
+}
+
+export function createRouter () {
+  return new Router(routerOptions)
+}
