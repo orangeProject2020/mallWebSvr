f860d11624a1f541cbc9abdc8154f504dd63b2a4
lucong <332553882@qq.com>
2019-11-27 20:19:31 +0800

    ad

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -3,27 +3,28 @@ import Router from 'vue-router'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
-const _51e7edf7 = () => interopDefault(import('../pages/about.vue' /* webpackChunkName: "pages/about" */))
-const _5243c3ae = () => interopDefault(import('../pages/auth.vue' /* webpackChunkName: "pages/auth" */))
-const _056e35b4 = () => interopDefault(import('../pages/cart.vue' /* webpackChunkName: "pages/cart" */))
-const _4d1a2ba4 = () => interopDefault(import('../pages/list.vue' /* webpackChunkName: "pages/list" */))
-const _1be0cb70 = () => interopDefault(import('../pages/payment.vue' /* webpackChunkName: "pages/payment" */))
-const _ebddd4c4 = () => interopDefault(import('../pages/protocol.vue' /* webpackChunkName: "pages/protocol" */))
-const _e8bc5542 = () => interopDefault(import('../pages/address/list.vue' /* webpackChunkName: "pages/address/list" */))
-const _4211a9ca = () => interopDefault(import('../pages/address/update.vue' /* webpackChunkName: "pages/address/update" */))
-const _fc6c94b6 = () => interopDefault(import('../pages/demo/page.vue' /* webpackChunkName: "pages/demo/page" */))
-const _10482b20 = () => interopDefault(import('../pages/download/app.vue' /* webpackChunkName: "pages/download/app" */))
-const _9370c7a0 = () => interopDefault(import('../pages/goods/detail.vue' /* webpackChunkName: "pages/goods/detail" */))
-const _769717da = () => interopDefault(import('../pages/invite/join.vue' /* webpackChunkName: "pages/invite/join" */))
-const _3612672e = () => interopDefault(import('../pages/order/confirm.vue' /* webpackChunkName: "pages/order/confirm" */))
-const _02b28138 = () => interopDefault(import('../pages/order/detail.vue' /* webpackChunkName: "pages/order/detail" */))
-const _8f44da76 = () => interopDefault(import('../pages/order/list.vue' /* webpackChunkName: "pages/order/list" */))
-const _36b01db6 = () => interopDefault(import('../pages/user/address.vue' /* webpackChunkName: "pages/user/address" */))
-const _5d687148 = () => interopDefault(import('../pages/user/info.vue' /* webpackChunkName: "pages/user/info" */))
-const _64887eb7 = () => interopDefault(import('../pages/user/invite.vue' /* webpackChunkName: "pages/user/invite" */))
-const _48610f1c = () => interopDefault(import('../pages/user/profit.vue' /* webpackChunkName: "pages/user/profit" */))
-const _19413690 = () => interopDefault(import('../pages/user/withdraw.vue' /* webpackChunkName: "pages/user/withdraw" */))
-const _719fc8bc = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
+const _16f48e03 = () => interopDefault(import('../pages/about.vue' /* webpackChunkName: "pages/about" */))
+const _505cf222 = () => interopDefault(import('../pages/auth.vue' /* webpackChunkName: "pages/auth" */))
+const _093bd8cc = () => interopDefault(import('../pages/cart.vue' /* webpackChunkName: "pages/cart" */))
+const _4b335a18 = () => interopDefault(import('../pages/list.vue' /* webpackChunkName: "pages/list" */))
+const _1ee38a0b = () => interopDefault(import('../pages/lists.vue' /* webpackChunkName: "pages/lists" */))
+const _5f74cf08 = () => interopDefault(import('../pages/payment.vue' /* webpackChunkName: "pages/payment" */))
+const _6245ea12 = () => interopDefault(import('../pages/protocol.vue' /* webpackChunkName: "pages/protocol" */))
+const _48a1605a = () => interopDefault(import('../pages/address/list.vue' /* webpackChunkName: "pages/address/list" */))
+const _76ab5184 = () => interopDefault(import('../pages/address/update.vue' /* webpackChunkName: "pages/address/update" */))
+const _9fa1209e = () => interopDefault(import('../pages/demo/page.vue' /* webpackChunkName: "pages/demo/page" */))
+const _6055a594 = () => interopDefault(import('../pages/download/app.vue' /* webpackChunkName: "pages/download/app" */))
+const _065516a4 = () => interopDefault(import('../pages/goods/detail.vue' /* webpackChunkName: "pages/goods/detail" */))
+const _bb130234 = () => interopDefault(import('../pages/invite/join.vue' /* webpackChunkName: "pages/invite/join" */))
+const _d2cebf16 = () => interopDefault(import('../pages/order/confirm.vue' /* webpackChunkName: "pages/order/confirm" */))
+const _52bffbac = () => interopDefault(import('../pages/order/detail.vue' /* webpackChunkName: "pages/order/detail" */))
+const _56af1a39 = () => interopDefault(import('../pages/order/list.vue' /* webpackChunkName: "pages/order/list" */))
+const _f284cfac = () => interopDefault(import('../pages/user/address.vue' /* webpackChunkName: "pages/user/address" */))
+const _009cfd30 = () => interopDefault(import('../pages/user/info.vue' /* webpackChunkName: "pages/user/info" */))
+const _df30347a = () => interopDefault(import('../pages/user/invite.vue' /* webpackChunkName: "pages/user/invite" */))
+const _07aedf7e = () => interopDefault(import('../pages/user/profit.vue' /* webpackChunkName: "pages/user/profit" */))
+const _b5fd8e78 = () => interopDefault(import('../pages/user/withdraw.vue' /* webpackChunkName: "pages/user/withdraw" */))
+const _36ac68c8 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
 
 Vue.use(Router)
 
@@ -36,87 +37,91 @@ export const routerOptions = {
 
   routes: [{
     path: "/about",
-    component: _51e7edf7,
+    component: _16f48e03,
     name: "about"
   }, {
     path: "/auth",
-    component: _5243c3ae,
+    component: _505cf222,
     name: "auth"
   }, {
     path: "/cart",
-    component: _056e35b4,
+    component: _093bd8cc,
     name: "cart"
   }, {
     path: "/list",
-    component: _4d1a2ba4,
+    component: _4b335a18,
     name: "list"
   }, {
+    path: "/lists",
+    component: _1ee38a0b,
+    name: "lists"
+  }, {
     path: "/payment",
-    component: _1be0cb70,
+    component: _5f74cf08,
     name: "payment"
   }, {
     path: "/protocol",
-    component: _ebddd4c4,
+    component: _6245ea12,
     name: "protocol"
   }, {
     path: "/address/list",
-    component: _e8bc5542,
+    component: _48a1605a,
     name: "address-list"
   }, {
     path: "/address/update",
-    component: _4211a9ca,
+    component: _76ab5184,
     name: "address-update"
   }, {
     path: "/demo/page",
-    component: _fc6c94b6,
+    component: _9fa1209e,
     name: "demo-page"
   }, {
     path: "/download/app",
-    component: _10482b20,
+    component: _6055a594,
     name: "download-app"
   }, {
     path: "/goods/detail",
-    component: _9370c7a0,
+    component: _065516a4,
     name: "goods-detail"
   }, {
     path: "/invite/join",
-    component: _769717da,
+    component: _bb130234,
     name: "invite-join"
   }, {
     path: "/order/confirm",
-    component: _3612672e,
+    component: _d2cebf16,
     name: "order-confirm"
   }, {
     path: "/order/detail",
-    component: _02b28138,
+    component: _52bffbac,
     name: "order-detail"
   }, {
     path: "/order/list",
-    component: _8f44da76,
+    component: _56af1a39,
     name: "order-list"
   }, {
     path: "/user/address",
-    component: _36b01db6,
+    component: _f284cfac,
     name: "user-address"
   }, {
     path: "/user/info",
-    component: _5d687148,
+    component: _009cfd30,
     name: "user-info"
   }, {
     path: "/user/invite",
-    component: _64887eb7,
+    component: _df30347a,
     name: "user-invite"
   }, {
     path: "/user/profit",
-    component: _48610f1c,
+    component: _07aedf7e,
     name: "user-profit"
   }, {
     path: "/user/withdraw",
-    component: _19413690,
+    component: _b5fd8e78,
     name: "user-withdraw"
   }, {
     path: "/",
-    component: _719fc8bc,
+    component: _36ac68c8,
     name: "index"
   }],
 
